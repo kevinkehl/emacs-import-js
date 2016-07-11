a4222a1a15c236952889edca1620d89a00f014de
Kevin Kehl <kevin.kehl@brigade.com>
2016-07-11 10:10:23 -0700

    WIP

--- plugin/import-js.el
+++ plugin/import-js.el
@@ -29,6 +29,7 @@
 ;;; Code:
 
 (require 'json)
+(require 'comint)
 
 (defvar import-js-buffer nil "Current import-js process buffer")
 (defvar import-js-project-root "." "Root of your project")
@@ -57,10 +58,17 @@
       module)))
 
 ;;;###autoload
+;; (defun import-js-import ()
+;;   (interactive)
+;;   (save-some-buffers)
+;;   (import-js-send-input "word" "--overwrite" (import-js-word-at-point)))
 (defun import-js-import ()
   (interactive)
   (save-some-buffers)
-  (import-js-send-input "word" "--overwrite" (import-js-word-at-point)))
+  (import-js-send-daemon-input (json-encode `((command . word)
+                                              (commandArg . ,(import-js-word-at-point))
+                                              (fileContent . ,(buffer-string))
+                                              (pathToFile . ,buffer-file-name)))))
 
 ;;;###autoload
 (defun import-js-fix ()
@@ -74,6 +82,34 @@
   (let ((goto-list (json-read-from-string
                     (import-js-send-input "goto" (import-js-word-at-point)))))
     (find-file (cdr (assoc 'goto goto-list)))))
+
+(defun import-js-send-daemon-input (json)
+  (comint-send-string import-js-buffer (concat json "\n")))
+
+(defun import-js-output-filter (output)
+  "Check if the current prompt is a top-level prompt."
+  (if (string-match "import:success" output)
+      (save-current-buffer
+        (set-buffer import-buffer)
+        (revert-buffer t t t)))
+  (if (string-match "goto:success:\\(.*\\)" output)
+      (save-current-buffer
+        (find-file (match-string 1 output)))))
+
+;;;###autoload
+(defun run-import-js ()
+  "Open a process buffer to run import-js"
+  (interactive)
+  (let ((command (concat "importjsd" " --parent-pid " (number-to-string (emacs-pid))))
+        (name "import-js"))
+    (if (not (comint-check-proc import-js-buffer))
+        (let ((commandlist (split-string-and-unquote command))
+              (process-environment process-environment))
+          (setenv "PAGER" (executable-find "cat"))
+          (set-buffer (apply 'make-comint name (car commandlist)
+                             nil (cdr commandlist)))))
+    (setq import-js-buffer (format "*%s*" name))
+    (add-hook 'comint-output-filter-functions 'import-js-output-filter nil t)))
 
 (provide 'import-js)
 ;;; import-js.el ends here
